22d50d0750c8039b98d20c38f25680569b40ba72
vmanamino <vmanamino@gmail.com>
2016-06-25 03:22:22 +0000

    hash map implementation

--- hash_maps_one.js
+++ hash_maps_one.js
@@ -1,29 +1,83 @@
-var example1 = 'madam';
+var HashMap = function(initialCapacity) {
+    this.length = 0;
+    this._slots = [];
+    this._capacity = initialCapacity || 8;
+    this._deleted = 0;
+};
+HashMap.MAX_LOAD_RATIO = 0.9;
+HashMap.SIZE_RATIO = 3;
 
-var length = example1.length;
-
-var inner = 0;
-
-var str = '';
-
-var instances = 0;
-
-var count = 0;
-
-while (count < length){
-  console.log(count);
-  inner = count;
-  while (inner < length){
-    str += example1.charAt(inner);
-    if (example1.charAt(count)==example1.charAt(inner)){
-      instances += 1;
+HashMap._hashString = function(string) {
+    var hash = 5381;
+    for (var i=0; i<string.length; i++) {
+        hash = (hash << 5) + hash + string.charCodeAt(i);
+        hash = hash & hash;
     }
-    inner++;
-  }
-  console.log(str);
-  console.log('char '+example1.charAt(count));
-  console.log(instances+' times');
-  instances = 0;
-  str = '';
-  count++;
-}
+    return hash >>> 0;
+};
+
+HashMap.prototype.get = function(key) {
+    var index = this._findSlot(key);
+    if (this._slots[index] === undefined) {
+        throw new Error('Key error');
+    }
+    return this._slots[index].value;
+};
+
+HashMap.prototype.set = function(key, value) {
+    var loadRatio = (this.length + this._deleted + 1) / this._capacity;
+    if (loadRatio > HashMap.MAX_LOAD_RATIO) {
+        this._resize(this._capacity * HashMap.SIZE_RATIO);
+    }
+
+    var index = this._findSlot(key);
+    this._slots[index] = {
+        key: key,
+        value: value,
+        deleted: false
+    };
+    this.length++;
+};
+
+HashMap.prototype.remove = function(key) {
+    var index = this._findSlot(key);
+    var slot = this._slots[index];
+    if (slot === undefined) {
+        throw new Error('Key error');
+    }
+    slot.deleted = true;
+    this.length--;
+    this._deleted++;
+};
+
+HashMap.prototype._findSlot = function(key) {
+    var hash = HashMap._hashString(key);
+    var start = hash % this._capacity;
+
+    for (var i=start; i<start + this._capacity; i++) {
+        var index = i % this._capacity;
+        var slot = this._slots[index];
+        if (slot === undefined || (slot.key == key && !slot.deleted)) {
+            return index;
+        }
+    }
+    // Unreachable
+};
+
+HashMap.prototype._resize = function(size) {
+    var oldSlots = this._slots;
+    this._capacity = size;
+    this._deleted = 0;
+    this._slots = [];
+    for (var i=0; i<oldSlots.length; i++) {
+        var slot = oldSlots[i];
+        if (slot !== undefined && !slot.deleted) {
+            this.set(slot.key, slot.value);
+        }
+    }
+};
+
+var hash_map = new HashMap();
+
+hash_map.set('m', 2)
+console.log(hash_map.get('m'))
