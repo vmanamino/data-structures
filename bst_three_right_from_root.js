ec3b974050b99a1dc0e90078754f2ce1a8ac8c05
vmanamino <vmanamino@gmail.com>
2016-07-03 17:08:05 +0000

    all combos of right from root

--- bst_three_right_from_root.js
+++ bst_three_right_from_root.js
@@ -110,109 +110,59 @@ BinarySearchTree.prototype._findMin = function() {
 };
 
 var bst = new BinarySearchTree(9, 9);
-// bst.insert(7, 7);
+bst.insert(7, 7);
 bst.insert(4, 4);
-// bst.insert(5, 5);
-// bst.insert(10, 10);
-bst.insert(8, 8);
 bst.insert(5, 5);
+bst.insert(10, 10);
+bst.insert(8, 8);
+// bst.insert(5, 5);
 // console.log(bst.key)
 // bst.insert(12, 12);
 // bst.insert(11, 11);
 // bst.insert(15, 15);
 // bst.insert(14, 14);
+// bst.insert(16, 16);
 var current = '';
 var message = '';
 var highest = 0;
 var count = 0;
-var obj = {}
+var obj = {};
 
-var crawl = function(node){
-  
-  if (node.right || node.left){
-    if (node.right){
-      if (node.right.value > highest){
-        
-        highest = node.right.value;
-      }
-      crawl(node.right)
-    }
-    if (node.left){
-      if (node.left.value > highest){
-        
-        highest = node.left.value;
-      }      
-      crawl(node.left)
-    }
-  }
-  else if (!(node.right && node.left) && node.parent){
-    if (node.value > highest){
-      
-      highest = node.value
-    }
-  }
-  return highest;
-}
-
-var thirdHighest = function(node){
-  var hoechstes = crawl(node);
-  return hoechstes;  
-}
 var second = '';
 var third = '';
 var climbDown = function(node){ 
-  //climb down right from root
+  //climb down right from root to the last node
   if (node.right){
-    console.log("current node "+node.key)
-    console.log("right node of current is "+node.right.key)
-    climbDown(node.right)
+    climbDown(node.right);
   }  
-  else {    
-    if (node.parent){ // right or left branches from root
-      if (node.parent.key > bst.key){ // keeping to right side of root node
-        if (node.left){
-          third = node.parent;
-        }
-        else if (node.parent.left){
+  else {       
+    if (node.parent.key > bst.key){ // keeping to right side of root node
+      if (node.left){
+        third = node.parent;
+      }
+      else if (node.parent.left){
+        third = node.parent.left;
+      }
+      else if (node.parent.parent){
+        third = node.parent.parent;
+      }
+    }  
+    else if (node.parent.key == bst.key){
+      if (node.parent.left){
+        if (!node.parent.left.right){
           third = node.parent.left;
         }
-        else if (node.parent.parent){
-          third = node.parent.parent;
-        }
-      }
-      else if (node.parent.key == bst.key){ // only one node branch on right from root
-        if (node.parent.left){          
-          if (node.parent.left.right){            
-            climbDown(node.parent.left)
-          }
-          else {
-            third = node.parent.left;
-          }
-        }        
-      }
-      else if (node.parent.key < bst.key && bst.right){ //right branches to climb left of root node
-        console.log('left side')
-        third = node;
-      }
-      else if (node.parent.key < bst.key && !bst.right){ // tree only has branches to the left of root
-        if (node.left){
-          third = node.left;
-        }
-        else {
-          third = node.parent;
+        else { // take first node left from root and climb down to last right node
+          climbDown(node.parent.left);      
         }
       }
     }
-    
-    else if (node.left.right && !node.parent){ // tree has only branches to the left of root
-//       console.log('right');
-//       console.log(node.left.right.key)
-      climbDown(node.left.right)
+    else if (node.parent.key < bst.key){
+      if (node.parent.right.key == node.key){
+        third = node;
+      }
     }
   }
   return third;
-}
-
-console.log(climbDown(bst).key)
-// console.log(bst.right.right.right.right.key)
-// console.log(thirdHighest(bst))
+};
+console.log(climbDown(bst).key);
